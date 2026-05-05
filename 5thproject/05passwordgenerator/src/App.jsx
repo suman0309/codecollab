ee936fa0076605a6eb331657b377b196b15e63c7
suman0309 <sumanmondal0612@gmail.com>
2026-05-05 07:49:16 +0530

    new hook callback and useEffect is integrated

--- 5thproject/05passwordgenerator/src/App.jsx
+++ 5thproject/05passwordgenerator/src/App.jsx
@@ -1,17 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useCallback, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [number, setNumber] = useState(false)
+  const [character, setCharacter] = useState(false)
+  const [password, setPassword] = useState("")
+
+// const passwordGenerator = useCallback(() => {
+                                  //function //set of variables
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (number) str += "0123456789"
+    if (character) str += "!@#$%^&*(){}[]?+_|"
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor((Math.random() * str.length)+1)
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+  }, [length, number, character,setPassword])
+
+//page refesh ya changes karne par without pressing buttton password genearte ho jaye so we use useEffect hook
+useEffect(()=>{passwordGenerator()},[length,number,character,passwordGenerator])
+
 
   return (
     <>
-     <div className="bg-blue-500 text-white text-3xl p-10">
-      NOW IT WILL WORK 🚀
-    </div>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800 text-orange-500'>
+        <h1 className='text-white text-center'>Password Generator</h1>
+        
+     
+
+      <div className="flex shadow rounded-lg overflow-hidden ">
+        <label className='py-1' >password :{password}</label>
+        {/* <input
+          type='text'
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='password'
+          readOnly
+        /> */}
+        
+      </div>
+
+<button 
+className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+
+<div className='flex text-sm gap-x-2'>
+  <div className='flex items-center gap-x-1'>
+
+<input 
+type="range"
+min={6}
+max={100}
+value={length}
+className='cursor-pointer'
+onChange={(e)=>{setLength(e.target.value)}}
+/>
+  <label>length: {length}</label>
+
+<div className='flex items-center  text-amber-50 gap-x-1'>
+  <input 
+  type='checkbox'
+  defaultChecked={number}
+  id='numberInput'
+  onChange={()=>{
+    setNumber((prev)=>!prev);  //on change jo bhi prev value tha t/f uska opposite
+  }}
+
+  />
+  <label htmlFor='numberInput' >Numberss</label>
+</div>
+<div className="flex items-center text-amber-50 gap-x-1">
+  <input
+    type="checkbox"
+    defaultChecked={character}
+    id="characterInput"
+    onChange={() => setCharacter((prev) => !prev)}
+  />
+  <label htmlFor="characterInput">Characters</label>
+</div>
+
+  </div>
+</div>
+      <button 
+        onClick={passwordGenerator}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Generate Password
+      </button>
+
+
+
+ </div>
+
+
     </>
   )
 }
